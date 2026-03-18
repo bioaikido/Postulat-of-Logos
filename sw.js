0471017a1f47fa32533ae8c6dda3c5f9f671310c
Dima <90134942+bioaikido@users.noreply.github.com>
2026-03-18 14:56:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'postulate-logos-v2';
+
+const PRECACHE = [
+  './',
+  './index.html',
+  './postulate-column.html',
+  './postulate-sudoku.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if(cached) return cached;
+      return fetch(e.request).then(response => {
+        if(!response || response.status !== 200 || response.type === 'opaque') return response;
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return response;
+      });
+    })
+  );
+});
